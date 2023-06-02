233f30c3f3a0164b2134dfb3a443eb6e3be35b76
Enow Divine <sirdivine16@gmail.com>
2023-06-02 18:12:27 +0100

    deleting blog post

--- articles/urls.py
+++ articles/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, re_path
 from . import views
 
-app_name = 'articles'
+app_name = "articles"
 
 urlpatterns = [
-    path('', views.article_list, name="list"),
-    path('create', views.article_create, name="create"),
-    re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail, name='article_detail'),
+    path("", views.article_list, name="list"),
+    path("create", views.article_create, name="create"),
+    path("delete/<id>", views.article_delete, name="delete"),
+    re_path(r"^(?P<slug>[\w-]+)/$", views.article_detail, name="article_detail"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -7,12 +7,19 @@ from . import forms
 # Create your views here.
 def article_list(request):
     articles = Article.objects.all().order_by('date')
+    print(articles)
     return render(request, 'articles/article_list.html', {'articles': articles})
 
 def article_detail(request, slug):
     # return HttpResponse(slug)
     article = Article.objects.get(slug=slug)
     return render(request, 'articles/article_detail.html', {'article': article})
+
+def article_delete(request, id=None):
+    # return HttpResponse(slug)
+    article = Article.objects.get(id=id)
+    article.delete()
+    return render(request, 'articles/delete_confirm.html')
 
 @login_required(login_url="/accounts/login") #login reuired to create article
 def article_create(request):
